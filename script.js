aab53bff02e131128039a46216a77722f0863af3
Ahmadjonov004 <ahmadjonovasilbek624@gmail.com>
2024-12-15 23:34:21 +0500

    7-misolgacha sihlab qoydim

---
+++ script.js
@@ -0,0 +1,46 @@
+//!! 1-misol Malumot turlari
+
+// let birthdayYear = 2004;
+// let NowYear = new Date().getFullYear();
+
+// console.log(NowYear - birthdayYear); 
+
+
+// !!2-misol 
+
+// let num = 123;
+// let text = "First Name"
+// console.log(typeof(num));
+// console.log(typeof(text));
+
+// !!3-misol
+
+// let valyuteName = "dollar";
+// let value = 100;
+
+// console.log(value + " " + valyuteName);
+
+
+// !!4-misol
+
+
+
+
+// !!5-misol
+
+// let num  = Number(prompt("sonni kiriting"));
+
+// console.log(Math.floor(num) + Number.isInteger(num));
+
+
+
+// !!6-misol numbers
+
+// let gbBuy = 5;
+
+// console.log((gbBuy * 12500) + " So'mlik internet ishlatdingiz")
+
+// !!7-misol numbers
+
+
+
