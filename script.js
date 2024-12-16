24c9c9c9b456ce3d09004336958ecc194544ff05
Ahmadjonov004 <ahmadjonovasilbek624@gmail.com>
2024-12-16 18:03:39 +0500

    muakkabgacha misollarni ishlab qoyfim

--- script.js
+++ script.js
@@ -42,5 +42,192 @@
 
 // !!7-misol numbers
 
+// let oil = 5;
+
+// console.log(((oil * 100) / 10) + " km masofani bosib o'tadi")
+
+// !!8-misol numbers
 
 
+// let num = Math.floor(Math.random() * 100);
+// console.log(num);
+
+// !! 9-misol numbers
+
+// let num = 20;
+// console.log(num % 5)
+
+// !!10-misol numbers
+
+// let doiraRadiusi = 17;
+// console.log(Math.PI * doiraRadiusi**2);
+
+
+// !! 11-misol Math
+
+// let narx =  +prompt("Narxni kiriting:")
+
+// console.log(Math.floor(narx) + "pastga qarab yaxlitlanganda")
+// console.log(Math.ceil(narx) + "yuqoriga qarab yaxlitlanganda")
+
+// !! 12-misol Math qolib ketdi
+
+// let doctorName = ("asilbek", "qahhorali", "samandar")
+
+// let name = doctorName(Math.floor(Math.indexof(2)));
+
+// !! 13-misol Math
+
+
+
+
+
+// !! 14-misol Math
+
+// let kubTomoni = 15;
+
+// console.log(Math.pow(kubTomoni, 3));
+
+// !! 15-misol Math
+
+// let numbers = (1, 2, 3, 4, 5, 6 );
+
+// console.log(Math.max(numbers));
+
+// !! 16-misol string
+
+// let name  = "asilbek";
+// let firstLetter =name.charAt(0).toUpperCase();
+// let qoganSozlar = name.slice(1);
+
+// console.log(firstLetter + qoganSozlar);
+
+//!! 17-misol string
+
+// let pochta = "ahmadjonovasilbek624@gmail.com";
+
+// console.log(pochta.slice(-9, -4))
+
+// !! 18-misol string
+
+// let telNumber = "+998976611747";
+
+// console.log(telNumber.slice(0, 4) + " " + telNumber.slice(4, 6) + " " + telNumber.slice(6, 9) + " " + telNumber.slice(9));
+
+
+// !! 19-misol string
+
+// let about = "Men Ahmadjonov Asilbek 2004-yil 6-oktabrda tugilganman";
+// console.log(about.match(/\d+/g));
+
+// !! 20-misol string
+
+// let words = "hammasi albatta yaxsh boladi"
+
+// let wordArray = words.split(" ");
+// console.log("sozlarin soni:" + wordArray.length)
+
+// !!21-misol Shart operatorlari (if else)
+
+// let words = "asilbek2004";
+
+// if (words.length >= 8) {
+//     console.log("To'g'ri parol");
+// }else{
+//     console.log("Noto'g'ri parol");
+// }
+
+
+//!! 22-misol Shart operatorlari (if else)
+
+// let volume = 51;
+
+// if (volume >= 50){
+//     console.log("ovoz juda baland");
+// }else{
+//     console.log("bemalol eshitsangiz bo'ladi");
+// }
+
+
+// !! 24-misol Shart operatorlari (if else)
+
+// let numbers = 22;
+
+// if (numbers % 2 == 0) {
+//     console.log("Juft son");
+// }else{
+//     console.log("Toq son");
+// }
+
+// !! 25-misol Shart operatorlari (if else)
+
+// let firstNumber = 34;
+// let secondNumber = 35;
+
+// if (firstNumber > secondNumber) {
+//     console.log("birinchi son katta");
+// }else{
+//     console.log("ikkinchi son katta");
+// }
+
+// !! 26-misol switch
+
+// let weekName = "dushanba";
+
+// switch (weekName) {
+//     case "dushanba": console.log(" bugun dushanba"); break;
+//     case "seshanba": console.log(" bugun seshanba"); break;
+//     case "chorshanba": console.log(" bugun chorshanba"); break;
+//     case "payshanba": console.log(" bugun payshanba"); break;
+//     case "juma": console.log(" bugun juma"); break;
+//     case  "shanba": console.log(" bugun shanba"); break;
+//     case "yakshanba": console.log(" bugun yakshanba"); break;
+// }
+
+// !! 27-misol switches
+
+// let language = "english";
+
+// switch (language) {
+//     case "russian": console.log("Привет мир!"); break;
+//     case "english": console.log("Hello, world!"); break;
+//     case "spanish": console.log("Hola, mundo!"); break;
+//     default: console.log("Bu til mavjud emas."); break;
+// }
+
+// !! 28-misol
+
+// let weather = "quyoshli";
+
+// switch (weather) {
+//     case "quyoshli": console.log("Bugun havo issiqroq boladi");break;
+//     case "shamol": console.log("bugun havo sovuqroq boladi");break;
+// }
+
+// !! 29-misol
+
+// let firstNumber = 22;
+// let secondNumber = 3;
+
+// let amal = "+";
+
+// switch (amal) {
+//     case "+": console.log(firstNumber + secondNumber); break;
+//     case "-": console.log(firstNumber - secondNumber); break;
+//     case "*": console.log(firstNumber * secondNumber); break;
+//     case "/": console.log(firstNumber / secondNumber); break;
+//     default: console.log("Bunaqa operator yoq"); break;
+// }
+
+// !! 30-misol
+
+// let question = "A";
+
+// switch (question) {
+//     case "A": console.log("alo baxo"); break;
+//     case "B": console.log("O'rtacha baxo"); break;
+//     case "C": console.log("past baho"); break;
+//     default: console.log("siz qatnashmadingiz"); break;
+// }
+
+// !! murakkab misollar 31-misol
